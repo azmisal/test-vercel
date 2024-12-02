5a64102e48794547c268987cc2bcd67e5101fa0f
azmisal <b20cs33@gectcr.ac.in>
2024-12-02 17:26:24 +0530

    try5

--- api/index.js
+++ api/index.js
@@ -6,14 +6,13 @@ app.use(express.json());
 
 // Import routes
 const hiiRoute = require("./routes/hii");
-const getdownRoute = require("./routes/getdown");
 
 // Base route
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Use routes
-// app.use("/hii", hiiRoute);
-// app.use("/getdown", getdownRoute);
+app.use("/hii", hiiRoute);
+
 
 // Export the app
 module.exports = app;
--- api/routes/hii.js
+++ api/routes/hii.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Define a GET route for /hii
 router.get("/", (req, res) => {
-  res.send("Hello from the /hii route!");
+  res.send("Hello from the /hii rlkhgvk oute!");
 });
 
 module.exports = router;
